5f58704d4c1e0fcc306d33fcaf8c878337c7a146
Alessandro Candido <candido.ale@gmail.com>
2024-07-03 10:55:41 +0200

    build: Update flake definition with flake parts

--- flake.nix
+++ flake.nix
@@ -2,54 +2,45 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = {
     self,
     nixpkgs,
-    devenv,
-    systems,
+    flake-parts,
     ...
-  } @ inputs: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    packages = forEachSystem (system: {
-      default =
-        nixpkgs.legacyPackages.${system}.poetry2nix.mkPoetryApplication
-        {
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.devenv.flakeModule];
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = {pkgs, ...}: {
+        packages.default = pkgs.poetry2nix.mkPoetryApplication {
           projectDir = self;
           preferWheels = true;
         };
-    });
 
-    devShells =
-      forEachSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            {
-              packages = with pkgs; [pre-commit poethepoet];
+        devenv.shells.default = {
+          packages = with pkgs; [pre-commit poethepoet];
 
-              languages.python = {
+          languages.python = {
+            enable = true;
+            poetry = {
+              enable = true;
+              install = {
                 enable = true;
-                poetry = {
-                  enable = true;
-                  install.enable = true;
-                  install.groups = ["dev" "tests"];
-                  install.allExtras = true;
-                };
-                version = "3.11.6";
+                groups = ["dev" "test"];
               };
-            }
-          ];
+            };
+          };
         };
-      });
-  };
+      };
+    };
 
   nixConfig = {
     extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
