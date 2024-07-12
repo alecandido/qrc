807116ed3b244b5927c5797567e64a9ba2a9ece5
Alessandro Candido <candido.ale@gmail.com>
2024-07-12 14:39:56 +0200

    test: Fix runcard location

--- run/sbatch.sh
+++ run/sbatch.sh
@@ -5,4 +5,4 @@ BASEDIR=$(realpath "$SLURM_SUBMIT_DIR")
 QIBOLAB_PLATFORMS=$(realpath "$BASEDIR/qibolab_platforms_qrc")
 export QIBOLAB_PLATFORMS
 
-poetry run qq auto "$BASEDIR/card.yml" -o "$BASEDIR/var/card" -f
+poetry run qq auto "$BASEDIR/run/card.yml" -o "$BASEDIR/var/card" -f
