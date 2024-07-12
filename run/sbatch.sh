96a4877dd87d73e5659d609426825bd882e34d04
Alessandro Candido <candido.ale@gmail.com>
2024-07-12 14:33:06 +0200

    test: Add batch script for slurm

---
+++ run/sbatch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#SBATCH -p qw5q_platinum
+
+BASEDIR=$(realpath "$SLURM_SUBMIT_DIR")
+QIBOLAB_PLATFORMS=$(realpath "$BASEDIR/qibolab_platforms_qrc")
+export QIBOLAB_PLATFORMS
+
+poetry run qq auto "$BASEDIR/card.yml" -o "$BASEDIR/var/card" -f
