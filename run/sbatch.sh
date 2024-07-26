f59bc09b09445a07486d69260f9d2785dc088e6d
Alessandro Candido <candido.ale@gmail.com>
2024-07-26 10:41:47 +0200

    test: Add sbatch script

---
+++ run/sbatch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#SBATCH -p qw5q_platinum
+#SBATCH -o var/debug.out
+
+BASEDIR=$(realpath "$SLURM_SUBMIT_DIR")
+QIBOLAB_PLATFORMS=$(realpath "$BASEDIR/qibolab_platforms_qrc")
+export QIBOLAB_PLATFORMS
+
+poetry run qq auto "$BASEDIR/run/card.yml" -o "$BASEDIR/var/spinq10q-zcu111" -f
