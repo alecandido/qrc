8ee001fad64652a795e1d2fd287e2893b98b2f46
Alessandro Candido <candido.ale@gmail.com>
2024-07-29 15:50:42 +0200

    feat: Add few common commands

--- qrc/cli.py
+++ qrc/cli.py
@@ -1,3 +1,4 @@
+import subprocess
 from pathlib import Path
 
 import click
@@ -5,7 +6,26 @@ import click
 from .calibrate import calibrate, setup
 
 
-@click.command("qrc")
+@click.group()
+def _base():
+    pass
+
+
+@_base.command()
 @click.argument("path", type=click.Path(path_type=Path))
 def run(path: Path):
     print(setup())
+
+
+@_base.command()
+def install():
+    subprocess.run("poetry install --with dev".split())
+
+
+@_base.command()
+def queue():
+    subprocess.run("watch -n1 queue".split())
+
+@_base.command()
+def term():
+    subprocess.run("export TERM='xterm'".split())
