2695440c538d74944a48899a9a47f573bdb930d2
Alessandro Candido <candido.ale@gmail.com>
2024-08-25 10:37:46 +0200

    fix: Switch to actual platform

--- run/cal.py
+++ run/cal.py
@@ -1,13 +1,16 @@
 import sys
 from pathlib import Path
+from pprint import pprint
 
 from qibocal.cli.report import report
 from qibocal.auto.execute import Executor
 
 path = Path(sys.argv[1]) / "cal"
 
-with Executor.open("myexec", platform="dummy", path=path, force=True) as e:
+with Executor.open("myexec", platform="qw11q", path=path, force=True) as e:
     ssc = e.single_shot_classification(nshots=1000)
-    print("\nfidelities:\n", ssc.results.fidelity, "\n")
+    print("\nfidelities:\n")
+    pprint(ssc.results.fidelity)
+    print()
 
 report(path)
