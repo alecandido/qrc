60391a862ae98e818d75b5da4af659e363deb56b
Alessandro Candido <candido.ale@gmail.com>
2024-08-27 14:34:40 +0200

    fix: Avoid updating parameters
    Because of removed characterization section

--- run/cal.py
+++ run/cal.py
@@ -7,7 +7,7 @@ from qibocal.auto.execute import Executor
 
 path = Path(sys.argv[1]) / "cal"
 
-with Executor.open("myexec", platform="qw11qD", path=path, force=True) as e:
+with Executor.open("myexec", platform="qw11qD", path=path, force=True, update=False) as e:
     ssc = e.single_shot_classification(nshots=1000)
     print("\nfidelities:\n")
     pprint(ssc.results.fidelity)
