2b0457ca9b1b6a702d55b866cb4f8763e3edc367
Alessandro Candido <candido.ale@gmail.com>
2024-12-05 19:39:26 +0100

    fix: Complete qblox development environment set up

--- run/cal.py
+++ run/cal.py
@@ -8,7 +8,9 @@ from qibocal.auto.execute import Executor
 path = Path(sys.argv[1]) / "cal"
 
 with Executor.open("myexec", platform="iqm5q", path=path, force=True) as e:
-    ssc = e.resonator_spectroscopy(nshots=1000)
+    ssc = e.resonator_spectroscopy(
+        nshots=1000, freq_width=1e6, freq_step=5e4, power_level="low"
+    )
     pprint(ssc.results)
     print()
 
