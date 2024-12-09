cd50136b532b63319a55d6f513ad925b0ba68bbf
Alessandro Candido <candido.ale@gmail.com>
2024-12-09 19:45:31 +0100

    fix: Test sequence generation with single pi-pulse

--- run/lab.py
+++ run/lab.py
@@ -3,9 +3,11 @@ from qibolab import create_platform, PulseSequence
 platform = create_platform("iqm5q")
 
 q0 = platform.natives.single_qubit[0]
+assert q0.RX is not None
 assert q0.MZ is not None
 
 sequence = PulseSequence()
+sequence |= q0.RX()
 sequence |= q0.MZ()
 
 res = platform.execute([sequence], nshots=1e3)
