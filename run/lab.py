9dd43bf2fd01faa265070946259932701b0c6bcd
Alessandro Candido <candido.ale@gmail.com>
2024-12-10 19:08:04 +0100

    test: Add sweeper to sample program

--- run/lab.py
+++ run/lab.py
@@ -1,4 +1,8 @@
-from qibolab import create_platform, PulseSequence
+from pathlib import Path
+import tempfile
+
+from qibolab import create_platform, PulseSequence, Parameter, Sweeper
+from qibolab._core.components.configs import LogConfig
 
 platform = create_platform("iqm5q")
 
@@ -7,9 +11,19 @@ assert q0.RX is not None
 assert q0.MZ is not None
 
 sequence = PulseSequence()
-sequence |= q0.RX()
+rx = q0.RX()
+sequence |= rx
 sequence |= q0.MZ()
 
-res = platform.execute([sequence], nshots=1e3)
+log = Path(tempfile.mkdtemp(prefix="qblox-"))
+res = platform.execute(
+    [sequence],
+    nshots=1e3,
+    updates=[{"log": LogConfig(path=log).model_dump()}],
+    sweepers=[
+        [Sweeper(parameter=Parameter.amplitude, range=(0, 1, 0.2), pulses=[rx[0][1]])]
+    ],
+)
 
 print(res)
+print(log)
