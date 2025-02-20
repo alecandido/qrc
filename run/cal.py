c7fc44721eff8a628814bb49850ca389e336b0cc
Alessandro Candido <candido.ale@gmail.com>
2025-02-20 17:25:26 +0100

    fix: Turn calibration script to single shot
    Rather than spectroscopy

--- run/cal.py
+++ run/cal.py
@@ -1,17 +1,14 @@
-import sys
 from pathlib import Path
-from pprint import pprint
+import rich
 
 from qibocal.cli.report import report
 from qibocal.auto.execute import Executor
 
-path = Path(sys.argv[1]) / "cal"
+path = Path(__file__).parents[1] / "var" / "cal"
 
 with Executor.open("myexec", platform="iqm5q", path=path, force=True) as e:
-    ssc = e.resonator_spectroscopy(
-        nshots=1000, freq_width=1e6, freq_step=5e4, power_level="low"
-    )
-    pprint(ssc.results)
+    ssc = e.single_shot_classification(nshots=1000)
+    rich.print(ssc.results)
     print()
 
 report(path)
