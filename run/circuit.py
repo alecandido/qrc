f2e612a9e3931b88d080204bbf3a9a45d6cf8c41
Alessandro Candido <candido.ale@gmail.com>
2025-08-20 20:04:09 +0200

    :fix: Simplify circuit executing script

--- run/circuit.py
+++ run/circuit.py
@@ -1,11 +1,14 @@
 import qibo
+from qibo import gates as g
 
 qibo.set_backend("qibolab", platform="iqm5q")
 
 c = qibo.Circuit(5)
-for n in range(c.nqubits):
-    c.add(qibo.gates.GPI2(n, phi=0.1))
-    c.add(qibo.gates.M(n))
+# for n in range(c.nqubits):
+for n in [0]:
+    # c.add(g.GPI2(n, phi=0.1))
+    c.add(g.X(n))
+    c.add(g.M(n))
 r = c(nshots=10)
 
 print(r.samples())
